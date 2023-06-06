7013e6f39ea161f9c30f319990e5f6f521845b3e
marcela-fernandez-inchcape <marcela.fernandez@inchcape.com>
2023-06-05 23:07:19 -0500

    Exercise 2 to second point

--- 2_Control Structures and Loops/Exercise_2.py
+++ 2_Control Structures and Loops/Exercise_2.py
@@ -1,2 +1,11 @@
 # Create a Python program that checks if a given number is prime or not. The user
-# should input a number, and the program should print whether it is prime or not.
+# should input a number, and the program should print whether it is prime or not.}
+
+def prime(num):
+    if num and num % 2 != 0:
+        return f"The number {num} is prime"
+    return f"The number {num} is not prime"
+    
+num = int(input('Write a number: '))
+
+print(prime(num))
