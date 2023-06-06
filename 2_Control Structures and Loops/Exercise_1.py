436c4de56546f2ef6b6f043ad0787a33f63ca86a
marcela-fernandez-inchcape <marcela.fernandez@inchcape.com>
2023-06-05 22:52:54 -0500

    First exercise point 2

--- 2_Control Structures and Loops/Exercise_1.py
+++ 2_Control Structures and Loops/Exercise_1.py
@@ -1,2 +1,11 @@
 # Write a Python program that prints the first 10 Fibonacci 
 # numbers using a loop.
+
+import sys
+
+def fibonacci(n):
+    if n==1 or n==2:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print('Ten first fibonacci numbers ', [fibonacci(i) for i in range(1,11)])
