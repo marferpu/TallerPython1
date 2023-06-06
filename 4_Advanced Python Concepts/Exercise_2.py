f5c4608b9572ffa0f59fb2d77c70c4937ae03c18
marcela-fernandez-inchcape <marcela.fernandez@inchcape.com>
2023-06-06 13:44:04 -0500

    exercise 2 to fourth point

--- 4_Advanced Python Concepts/Exercise_2.py
+++ 4_Advanced Python Concepts/Exercise_2.py
@@ -1,2 +1,17 @@
 # Create a Python program that reads a text file and counts the occurrences of each
 # word using a dictionary. The program should print the words and their counts.
+
+def text_file_reader(text):
+    array_text = text.split(' ')
+    counter_word = {}
+    value = 1
+    for word in array_text:
+        if word in counter_word:
+            counter_word[word] += 1
+        else:
+            counter_word[word] = 1
+
+    print('Counter_word is: ', counter_word)
+
+message = 'hola como estas hola como estas estas bien o que'
+text_file_reader(message)
