193f5390ff5f8977ac431f0e321ec4474aabfd4c
marcela-fernandez-inchcape <marcela.fernandez@inchcape.com>
2023-06-06 14:48:37 -0500

    Exercise 2 to point 5

--- 5_Python Programming Best Practices/Exercise_2.py
+++ 5_Python Programming Best Practices/Exercise_2.py
@@ -1,3 +1,22 @@
 # Create a Python decorator called timer that measures the time taken to execute a
 # function. Use this decorator on a function that sorts a list of random numbers and
 # prints the sorted list.
+
+import datetime
+import random
+
+def timer(list):
+    time_init = datetime.datetime.now()
+    sorting_list(list)
+    time_end = datetime.datetime.now()
+    time = time_end - time_init
+    print(f'The time taken to execute the function was {time}')
+
+def sorting_list(list):
+    list.sort()
+    print(f'The sorted list is: ### {list} ###')
+
+list_desorted = random.sample(range(1, 100), 25)
+print(f'The desorded list is: *** {list_desorted} ***')
+
+timer(list_desorded)
