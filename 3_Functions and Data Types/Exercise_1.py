1743b3eea877ffe9da063e63ed62a4187ab1e150
marcela-fernandez-inchcape <marcela.fernandez@inchcape.com>
2023-06-05 23:23:12 -0500

    Exercises to third point

--- 3_Functions and Data Types/Exercise_1.py
+++ 3_Functions and Data Types/Exercise_1.py
@@ -1,2 +1,14 @@
 # Write a Python function called find_maximum that takes a list of numbers as input
 # and returns the maximum number from the list.
+
+def find_maximum(array):
+    max_num = array[0]
+    for num in array:
+        if num > max_num:
+            max_num = num
+    return max_num
+
+array_test = [1,6,3,4]
+print(f' The maximum number is {find_maximum(array_test)}')
+    
+    
--- 3_Functions and Data Types/Exercise_2.py
+++ 3_Functions and Data Types/Exercise_2.py
@@ -1,2 +1,14 @@
 # Create a Python function called reverse_string that takes a string as input and returns
 # the reversed string.
+
+def reverse_string(word):
+    if word:
+        words = word.split()
+        word_reverse = word[-1] + reverse_string(word[:-1])
+        return word_reverse
+    return ""
+
+string = input('Write a word: ')
+reverse_word = reverse_string(string)
+
+print('Your reverse word is: ', reverse_word)
