ff1b2310a0d609f4fc4aa39a42f60a6dca888042
marcela-fernandez-inchcape <marcela.fernandez@inchcape.com>
2023-06-06 14:25:36 -0500

    Exercise 1 to fifth point

--- 5_Python Programming Best Practices/Exercise_1.py
+++ 5_Python Programming Best Practices/Exercise_1.py
@@ -1,3 +1,30 @@
 # Write a Python function called is_palindrome that checks if a given word is a
 # palindrome. The function should have proper error handling and be tested with
 # unittest.
+
+class InvalidLengthWord(Exception):
+    pass
+
+def is_palindrome(word):
+    length_word = len(word)
+    if length_word % 2 != 0:
+        array_word = word.split()
+        middle = int(round((length_word -1)//2))
+        # print(f'middle: {middle}')
+        counter = 0
+        for i in range(0,middle):
+            if array_word[i] == array_word[-i]:
+                counter +=1
+        if counter == middle:
+            print (f'The word **{word}** is a palindrome')
+        else:
+             print (f'The word **{word}** is not a palindrome' )
+    else:
+        raise InvalidLengthWord(f'The word **{word}** is not a palindrome')  
+
+
+w = 'oso'
+is_palindrome(w)
+print('************************** ################### *********************************')
+w = 'osoris'
+is_palindrome(w)
